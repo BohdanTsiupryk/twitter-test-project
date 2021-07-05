9089847654ff026186d5dc72ef1ad9b65613ad2a
Bohdan Tsiupryk <bohdan.tsiupryk12@gmail.com>
2021-07-05 20:12:05 +0300

    initial

---
+++ src/main/java/bts/twitter/TwitterApplication.java
@@ -0,0 +1,13 @@
+package bts.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApplication.class, args);
+	}
+
+}
